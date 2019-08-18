eb7f3a54aa3a2e25009fb9d6cdc71909a9cc598d
Mario Villalobos <mariocr1611@gmail.com>
2019-08-17 20:36:16 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,7 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "AIzaSyBZ9J8bdkebKJ4F_SRACXV6iifSs1VanzI",
-    authDomain: "ddinit-8a8d2.firebaseapp.com",
-    databaseURL: "https://ddinit-8a8d2.firebaseio.com",
-    projectId: "ddinit-8a8d2",
-    storageBucket: "ddinit-8a8d2.appspot.com",
-    messagingSenderId: "994174756885",
-    appId: "1:994174756885:web:6647429660257497"
+
   }
 };
 
